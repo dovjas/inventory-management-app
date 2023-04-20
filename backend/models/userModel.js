484c55f362cb6817573edd34fb8df2e95fbfe74e
Dovydas J <dovydas.jaseliunas@gmail.com>
2023-04-20 20:28:50 +0300

    created User schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Please enter your name']
+    },
+    email:{
+        type:String,
+        required:[true, 'Please add your email'],
+        trim:true,
+        unique:true,
+        match:[
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid password"
+        ]
+    },
+    password:{
+        type:String,
+        required:[true,'The password is required'],
+        minLenght:[7,'The minimum amount of characters is 7'],
+        maxLenght:[24,'The maximum amount of characters is 24']
+    },
+    photo:{
+        type:String,
+        required:[true,'Please add your photo'],
+        default:'https://cdn-icons-png.flaticon.com/512/149/149071.png?w=740&t=st=1682011143~exp=1682011743~hmac=1560001dcfdc542bbd3f3446c7ae3d7a72a830be4f9baafceb0a17835c491605'
+    },
+    phone:{
+        type:String,
+        default:'+370'
+    },
+    bio:{
+        type:String,
+        maxLenght:[200,'Bio cannot be longer than 200 character'],
+        default:'bio',
+    },
+},{
+    timestamps:true,
+})
+
+const User = mongoose.model('User',userSchema);
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,19 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 
-const app = express();
+const app = express(); 
+// Midllewares
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
+// Routes
+app.get('/',(req,res)=>{
+    res.send('Homepage');
+});
+
+// Connect to DB and start server
 const PORT = process.env.PORT||5000
-
 mongoose
     .connect(process.env.MONGO_URI)
     .then(()=>{
