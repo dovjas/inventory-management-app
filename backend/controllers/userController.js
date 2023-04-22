93ca1fa997962d2fe3c3f8f5a4ee87e8ac086f35
Dovydas J <dovydas.jaseliunas@gmail.com>
2023-04-22 10:35:04 +0300

    added user passwod hash

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,49 @@
+const User = require("../models/userModel");
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
 
-const userRegister = (req,res) =>{
-    if(!req.body.email){
-        res.status(400)
-        throw new Error("Please add an email")
-    }
+const userRegister = asyncHandler(
+    async(req,res)=>{
+        const {name, email, password} = req.body;
 
-    res.send("User register page");
-};
+        if(!name||!email||!password){
+            res.status(400)
+            throw new Error("Please fill in all fields")
+        };
+        if(password.lenght < 7){
+            res.status(400)
+            throw new Error("Password can't be shorte than 7 chars")
+        };
 
-module.exports={
-    userRegister,
+    //Check if user already exists
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400);
+        throw new Error("This user already exists")
+    };
+    //Encrypt password before saving to DB
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+    //Create a new user
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword,
+    })
+    if(user){
+        const {_id, name, email, password, phone, bio} = user
+        res.status(201).json({
+            _id, name, email, password, phone, bio
+        })  
+    }else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }});
+
+
+
+module.exports= {
+   userRegister,
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { userRegister } = require("../controllers/userController");
 const router = express.Router();
 
-
 router.post('/register', userRegister);
 
 module.exports = router;
